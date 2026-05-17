dcc5805a9d6ee6bb0e8fbf45878fc81bfb0f67a6
Yasantha Niroshan <niroshanyi@gmail.com>
2026-05-18 01:56:21 +0530

    Added Minor Update - Added smaller test cases for Types,Type,LitList,SubProgs,Fcn,Params,Dclns,Dcln,Body,Statement - Looks like some of them are failing :)

--- tests/unit/test_parser.cpp
+++ tests/unit/test_parser.cpp
@@ -100,6 +100,135 @@ INSTANTIATE_TEST_SUITE_P(
         return info.param.name;
     });
 
+// Types  ->              => "types"
+//       | 'type' (Type ';')+ => "types"
+// Type   -> Name '=' LitList => "type"
+// LitList -> '(' Name list ',' ')' => "lit"
+INSTANTIATE_TEST_SUITE_P(
+    Types, ParserGrammarFileTest,
+    ::testing::Values(
+        GrammarFileCase{"types_empty", "types_empty"},
+        GrammarFileCase{"types_one", "types_one"},
+        GrammarFileCase{"types_two", "types_two"},
+        GrammarFileCase{"types_three", "types_three"},
+        GrammarFileCase{"types_lit_two", "types_lit_two"},
+        GrammarFileCase{"types_lit_three", "types_lit_three"},
+        GrammarFileCase{"types_lit_four", "types_lit_four"},
+        GrammarFileCase{"types_four", "types_four"},
+        GrammarFileCase{"types_two_lit_sizes", "types_two_lit_sizes"},
+        GrammarFileCase{"types_with_consts", "types_with_consts"}),
+    [](const ::testing::TestParamInfo<GrammarFileCase>& info) {
+        return info.param.name;
+    });
+
+// SubProgs -> Fcn* => "subprogs"
+// Fcn -> 'function' Name '(' Params ')' ':' Name ';' Consts Types Dclns Body Name ';'
+INSTANTIATE_TEST_SUITE_P(
+    SubProgs, ParserGrammarFileTest,
+    ::testing::Values(
+        GrammarFileCase{"subprogs_empty", "subprogs_empty"},
+        GrammarFileCase{"subprogs_one", "subprogs_one"},
+        GrammarFileCase{"subprogs_two", "subprogs_two"},
+        GrammarFileCase{"subprogs_three", "subprogs_three"},
+        GrammarFileCase{"subprogs_four", "subprogs_four"},
+        GrammarFileCase{"subprogs_fcn_types", "subprogs_fcn_types"},
+        GrammarFileCase{"subprogs_fcn_consts", "subprogs_fcn_consts"},
+        GrammarFileCase{"subprogs_fcn_dclns", "subprogs_fcn_dclns"},
+        GrammarFileCase{"subprogs_params_two", "subprogs_params_two"}),
+    [](const ::testing::TestParamInfo<GrammarFileCase>& info) {
+        return info.param.name;
+    });
+
+// Dclns -> | 'var' (Dcln ';')+ => "dclns"
+// Dcln  -> Name list ',' ':' Name => "var"
+INSTANTIATE_TEST_SUITE_P(
+    Dclns, ParserGrammarFileTest,
+    ::testing::Values(
+        GrammarFileCase{"dclns_empty", "dclns_empty"},
+        GrammarFileCase{"dclns_one", "dclns_one"},
+        GrammarFileCase{"dclns_two", "dclns_two"},
+        GrammarFileCase{"dclns_three", "dclns_three"},
+        GrammarFileCase{"dcln_names_two", "dcln_names_two"},
+        GrammarFileCase{"dcln_names_three", "dcln_names_three"}),
+    [](const ::testing::TestParamInfo<GrammarFileCase>& info) {
+        return info.param.name;
+    });
+
+// Params -> Dcln list ';' => "params"
+INSTANTIATE_TEST_SUITE_P(
+    Params, ParserGrammarFileTest,
+    ::testing::Values(
+        GrammarFileCase{"params_one", "params_one"},
+        GrammarFileCase{"params_two", "params_two"},
+        GrammarFileCase{"params_names_two", "params_names_two"},
+        GrammarFileCase{"params_names_three", "params_names_three"}),
+    [](const ::testing::TestParamInfo<GrammarFileCase>& info) {
+        return info.param.name;
+    });
+
+// Fcn -> 'function' Name '(' Params ')' ':' Name ';' Consts Types Dclns Body Name ';' => "fcn"
+INSTANTIATE_TEST_SUITE_P(
+    Fcn, ParserGrammarFileTest,
+    ::testing::Values(
+        GrammarFileCase{"fcn_minimal", "fcn_minimal"},
+        GrammarFileCase{"fcn_with_consts", "fcn_with_consts"},
+        GrammarFileCase{"fcn_with_types", "fcn_with_types"},
+        GrammarFileCase{"fcn_with_dclns", "fcn_with_dclns"},
+        GrammarFileCase{"fcn_with_body", "fcn_with_body"},
+        GrammarFileCase{"fcn_full", "fcn_full"}),
+    [](const ::testing::TestParamInfo<GrammarFileCase>& info) {
+        return info.param.name;
+    });
+
+// Body -> 'begin' Statement list ';' 'end' => "block"
+INSTANTIATE_TEST_SUITE_P(
+    Body, ParserGrammarFileTest,
+    ::testing::Values(
+        GrammarFileCase{"body_empty", "body_empty"},
+        GrammarFileCase{"body_one", "body_one"},
+        GrammarFileCase{"body_two", "body_two"},
+        GrammarFileCase{"body_three", "body_three"},
+        GrammarFileCase{"body_nested", "body_nested"}),
+    [](const ::testing::TestParamInfo<GrammarFileCase>& info) {
+        return info.param.name;
+    });
+
+// Statement -> assign | output | if | while | repeat | for | loop | case | read |
+//              exit | return | Body | <null>
+INSTANTIATE_TEST_SUITE_P(
+    Statement, ParserGrammarFileTest,
+    ::testing::Values(
+        GrammarFileCase{"stmt_assign", "stmt_assign"},
+        GrammarFileCase{"stmt_swap", "stmt_swap"},
+        GrammarFileCase{"stmt_output_expr", "stmt_output_expr"},
+        GrammarFileCase{"stmt_output_string", "stmt_output_string"},
+        GrammarFileCase{"stmt_output_two", "stmt_output_two"},
+        GrammarFileCase{"stmt_if", "stmt_if"},
+        GrammarFileCase{"stmt_if_else", "stmt_if_else"},
+        GrammarFileCase{"stmt_while", "stmt_while"},
+        GrammarFileCase{"stmt_repeat_one", "stmt_repeat_one"},
+        GrammarFileCase{"stmt_repeat_two", "stmt_repeat_two"},
+        GrammarFileCase{"stmt_repeat_null", "stmt_repeat_null"},
+        GrammarFileCase{"stmt_for_full", "stmt_for_full"},
+        GrammarFileCase{"stmt_for_null_init", "stmt_for_null_init"},
+        GrammarFileCase{"stmt_for_true_exp", "stmt_for_true_exp"},
+        GrammarFileCase{"stmt_for_null_step", "stmt_for_null_step"},
+        GrammarFileCase{"stmt_loop_one", "stmt_loop_one"},
+        GrammarFileCase{"stmt_loop_two", "stmt_loop_two"},
+        GrammarFileCase{"stmt_loop_null", "stmt_loop_null"},
+        GrammarFileCase{"stmt_case_one", "stmt_case_one"},
+        GrammarFileCase{"stmt_case_two", "stmt_case_two"},
+        GrammarFileCase{"stmt_case_range", "stmt_case_range"},
+        GrammarFileCase{"stmt_case_label_list", "stmt_case_label_list"},
+        GrammarFileCase{"stmt_case_otherwise", "stmt_case_otherwise"},
+        GrammarFileCase{"stmt_read_one", "stmt_read_one"},
+        GrammarFileCase{"stmt_read_two", "stmt_read_two"},
+        GrammarFileCase{"stmt_exit", "stmt_exit"},
+        GrammarFileCase{"stmt_return", "stmt_return"}),
+    [](const ::testing::TestParamInfo<GrammarFileCase>& info) {
+        return info.param.name;
+    });
+
 class ParserGrammarTest : public ::testing::Test {};
 
 TEST_F(ParserGrammarTest, StringNode_UnterminatedFailsTokenize) {
